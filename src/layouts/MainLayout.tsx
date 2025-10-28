cde88a621f067408a208062e6a804903fefb08f6
Ahmad Damar <ahmatdamar506@gmail.com>
2025-10-28 20:03:26 +0700

    menambahkan komponen scroll to top

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollTop";
 
 export default function MainLayout() {
     return (
         <div className="flex flex-col min-h-screen">
+            <ScrollToTop />
             <Navbar />
             <main className="flex-1 pt-16">
                 <Outlet />
